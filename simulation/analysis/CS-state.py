da4ae7de31d93321cdb9bb48e0577fb55f4210a7
Jonas <jonasbue@stud.ntnu.no>
2021-09-21 15:44:13 +0200

    Adds comments and does some minor code cleanup.

--- simulation/analysis/CS-state.py
+++ simulation/analysis/CS-state.py
@@ -1,6 +1,12 @@
+#########################################################
+# This script takes a log file from LAMMPS and plots    #
+# its compressibility factor vs. the Carnahan-Starling  #
+# equation of state.                                    #
+#########################################################
+
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import logfiles
 import convert_LAMMPS_output as convert
 
 # Increase font size in plots
@@ -8,6 +14,7 @@ font = {
     "size"  : "22",
 }
 plt.rc("font", **font)
+
 
 def Z(p, V, N, T, k=1):
     """ Computes compressibility factor of a system.
@@ -25,6 +32,7 @@ def Z(p, V, N, T, k=1):
     """
     return p*V/(N*k*T)
 
+
 def Z_Carnahan_Starling(n):
     """ Returns expected compressibility factor
         based on the Carnahan-Starling EoS,
@@ -36,54 +44,6 @@ def Z_Carnahan_Starling(n):
     """
     return (1 + n + n**2 - n**3) / (1 - n)**3
 
-def get_variable_indices(header, variables):
-    """ Searches the header of a log file for 
-        indices of different variables.
-        Inputs:
-            header:     headline of thermo output table, np.array.
-            variables:  list of variable names as strings/chars.
-                        Can include the following:
-                            pressure:       "p"
-                            volume:         "V"
-                            temperature:    "T"
-        Returns:
-            indices:    np array containing indices of the
-                        variables in the same order as they
-                        were given in the input argument.
-    """
-    var_dict = {
-        "Press"     : "p",
-        "Volume"    : "V",
-        "Temp"      : "T",
-    }
-    indices = np.zeros_like(variables, dtype=int)
-    for (i, var) in enumerate(header):
-        if var in var_dict.keys():
-            n = variables.index(var_dict[var])
-            indices[n] = i
-    return indices
-
-
-def load_system(filename, variables):
-    """ Loads a LAMMPS logfile, and returns
-        thermodynamical variables from it,
-        as a numpy array.
-        Inputs:
-            filename:   name of logfile, string.
-        Returns:
-            log:        Array of logged values from LAMMPS.
-                        First index selects the variable,
-                        second index selects the timestep.
-            indices:    
-    """
-    convert.convert_log_to_csv(filename)
-    log = pd.read_csv(
-        filename + ".csv", 
-        skiprows=2, 
-        delimiter=","
-    )
-    log = np.array(log)
-    return log.transpose()
 
 def plot_Z(p, V, T, eta):
     """ Plots compressibility factor of the system,
@@ -101,26 +61,20 @@ def plot_Z(p, V, T, eta):
         "o", label=f"$\eta$ = {eta}"
     )                          
 
-def unpack_varables(log_table, filename, variables):
-    """ Unpacks specified variables from log table 
-        with a header from a LAMMPS log file.
-    """
-    headers = np.array(
-        pd.read_csv(filename+".csv", nrows=0).columns
-    )
-    indices = get_variable_indices(headers, variables)
-    return log_table[indices]
 
 
 N = 1000
 eta_list = np.array([0.01, 0.1, 0.2, 0.3, 0.4, 0.5])
 variable_list = ["p", "V", "T"]
 
+
 # Calculate values of Z from measured p, V and T.
 for eta in eta_list:
     filename = f"log.eta_{eta}.lammps"
-    log_table = load_system( filename, variable_list)
-    pvt = unpack_varables( log_table, filename, variable_list)
+    constants = convert.extract_constants_from_log(filename)
+    print(constants)
+    log_table = logfiles.load_system(filename)
+    pvt = logfiles.unpack_varables(log_table, filename, variable_list)
     plot_Z(
         np.mean(pvt[variable_list.index("p")]), 
         np.mean(pvt[variable_list.index("V")]), 
---
+++ simulation/analysis/logfiles.py
@@ -0,0 +1,90 @@
+#####################################################
+# This file contains functions to load a log file   #
+# from LAMMPS, and convert it into useful data.     #
+# The conversion from log.lammps to .csv uses       #
+# functions provided by Christopher, while the      #
+# functions below extract names from the header     #
+# and provide a simple wrapping for the             #
+# .csv-conversion functions.                        #
+#####################################################
+
+import numpy as np
+import pandas as pd
+import convert_LAMMPS_output as convert
+
+
+def load_system(filename):
+    """ Loads a LAMMPS logfile, and returns
+        thermodynamical variables from it,
+        as a numpy array.
+        Inputs:
+            filename:   name of logfile, string.
+        Returns:
+            log_table:  Array of logged values from LAMMPS.
+                        First index selects the variable,
+                        second index selects the timestep.
+    """
+    convert.convert_log_to_csv(filename)
+    log_table = pd.read_csv(
+        filename + ".csv", 
+        skiprows=2, 
+        delimiter=","
+    )
+    log_table = np.array(log_table)
+    return log_table.transpose()
+
+
+def get_variable_indices(header, variables):
+    """ Searches the header of a log file for 
+        indices of different variables.
+        Inputs:
+            header:     headline of thermo output table, np.array.
+            variables:  list of variable names as strings/chars.
+                        Can include the following:
+                            pressure:       "p"
+                            volume:         "V"
+                            temperature:    "T"
+        Returns:
+            indices:    np array containing indices of the
+                        variables in the same order as they
+                        were given in the input argument.
+    """
+    var_dict = {
+        "Press"     : "p",
+        "Volume"    : "V",
+        "Temp"      : "T",
+    }
+    indices = np.zeros_like(variables, dtype=int)
+    for (i, var) in enumerate(header):
+        if var in var_dict.keys():
+            n = variables.index(var_dict[var])
+            indices[n] = i
+    return indices
+
+
+def unpack_varables(log_table, filename, variables):
+    """ Unpacks specified variables from log table 
+        with a header from a LAMMPS log file.
+
+        This assumes that load_system(filename)
+        has already been run on this filename,
+        so that there exists a .csv file and
+        a log_table for the correct simulation run.
+
+        Inputs:
+            log_table:  Array of logged values from LAMMPS.
+                        First index selects the variable,
+                        second index selects the timestep.
+            filename:   name of logfile, string.
+            variables:  list of variable names as strings/chars.
+                        Can include the following:
+                            pressure:       "p"
+                            volume:         "V"
+                            temperature:    "T"
+            
+    """
+    headers = np.array(
+        pd.read_csv(filename+".csv", nrows=0).columns
+    )
+    indices = get_variable_indices(headers, variables)
+    return log_table[indices]
